225dcebe985508a5fac6de65f2068746e68cdcf6
MIYABETaisei <mytspe46@gmail.com>
2022-08-01 12:31:52 +0900

    ✨ add markers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,21 @@ import mapboxgl from "mapbox-gl";
 import { useEffect, useRef } from "react";
 
 type Marker = {
-  city: string;
-  country: string;
+  name: string;
   latCoord: number;
   longCoord: number;
 };
 
 export const markers: Marker[] = [
   {
-    city: "Sydney",
-    country: "Australia",
-    latCoord: 33.64679726505224,
-    longCoord: 130.67388822375185,
+    name: "天神駅",
+    latCoord: 33.5914,
+    longCoord: 130.3989,
+  },
+  {
+    name: "博多駅",
+    latCoord: 33.5897,
+    longCoord: 130.4207,
   },
 ];
 
@@ -24,6 +27,9 @@ const Home: NextPage = () => {
   const geojson = {
     type: "Feature",
     features: markers.map((marker) => ({
+      properties: {
+        name: marker.name,
+      },
       geometry: {
         type: "Point",
         coordinates: {
@@ -38,7 +44,7 @@ const Home: NextPage = () => {
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [130.67388822375185, 33.64679726505224], // center map on Chad
+      center: [130.4017, 33.5902], // center map on Chad
       zoom: 15,
     });
     map.current.on("load", () => {
@@ -46,10 +52,18 @@ const Home: NextPage = () => {
         // create a DOM element for the marker
         const markerIcon = document.createElement("div");
         markerIcon.className =
-          "w-[30px] h-[30px] rounded-full bg-emerald-500 border-2 border-white";
+          "w-[30px] h-[30px] rounded-full bg-pink-500 border-2 border-white";
 
         new mapboxgl.Marker(markerIcon)
           .setLngLat(marker.geometry.coordinates)
+          .setPopup(
+            // add pop out to map
+            new mapboxgl.Popup({ offset: 25 }).setHTML(
+              `<p style="font-size: 15px; ">Name: ${marker.properties.name}</p>
+              <button style="font-size: 15px; color: #fff; background-color: #000; width: 100%; padding: 10px; margin-top: 20px; border-radius: 10px;">Evaluate</button>
+              `
+            )
+          )
           .addTo(map.current);
       });
     });
