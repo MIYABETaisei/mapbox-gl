d42a3c8ff7b7270e0613fc607da50aab818f30a7
MIYABETaisei <mytspe46@gmail.com>
2022-08-21 17:15:34 +0900

    ✨ firebaseの初期化

---
+++ src/lib/firebase/init.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+// FirebaseApp インスタンスを初期化する
+initializeApp({
+  apiKey: "AIzaSyCwBCsfwID_YnaLHbO6tPylW3Z3ND2raa0",
+  authDomain: "gourmet-app-f68c8.firebaseapp.com",
+  projectId: "gourmet-app-f68c8",
+  storageBucket: "gourmet-app-f68c8.appspot.com",
+  messagingSenderId: "356271558942",
+  appId: "1:356271558942:web:42b78c52071bfa70f5390f",
+  measurementId: "G-0J2EHKQKM9",
+});
